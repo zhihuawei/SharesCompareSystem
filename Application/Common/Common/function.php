67082434f87e748ad0140843351031fde13fc6af
h我叫小刀 <214564382@qq.com>
2017-11-29 16:13:55 +0800

    主文件提交

---
+++ Application/Common/Common/function.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/28
+ * Time: 14:48
+ */
+/**
+ * CURL请求
+ * @param $url 请求url地址
+ * @param $method 请求方法 get post
+ * @param null $postfields post数据数组
+ * @param array $headers 请求header信息
+ * @param bool|false $debug 调试开启 默认false
+ * @return mixed
+ */
+function httpRequest($url, $method, $postfields = null, $headers = array(), $debug = false)
+{
+    $method = strtoupper($method);
+    $ci = curl_init();
+    /* Curl settings */
+    curl_setopt($ci, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+    curl_setopt($ci, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0");
+    curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, 60); /* 在发起连接前等待的时间，如果设置为0，则无限等待 */
+    curl_setopt($ci, CURLOPT_TIMEOUT, 7); /* 设置cURL允许执行的最长秒数 */
+    curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
+    switch ($method) {
+        case "POST":
+            curl_setopt($ci, CURLOPT_POST, true);
+            if (!empty($postfields)) {
+                $tmpdatastr = is_array($postfields) ? http_build_query($postfields) : $postfields;
+                curl_setopt($ci, CURLOPT_POSTFIELDS, $tmpdatastr);
+            }
+            break;
+        default:
+            curl_setopt($ci, CURLOPT_CUSTOMREQUEST, $method); /* //设置请求方式 */
+            break;
+    }
+    $ssl = preg_match('/^https:\/\//i', $url) ? TRUE : FALSE;
+    curl_setopt($ci, CURLOPT_URL, $url);
+    if ($ssl) {
+        curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, FALSE); // https请求 不验证证书和hosts
+        curl_setopt($ci, CURLOPT_SSL_VERIFYHOST, FALSE); // 不从证书中检查SSL加密算法是否存在
+    }
+    //curl_setopt($ci, CURLOPT_HEADER, true); /*启用时会将头文件的信息作为数据流输出*/
+    curl_setopt($ci, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ci, CURLOPT_MAXREDIRS, 2);/*指定最多的HTTP重定向的数量，这个选项是和CURLOPT_FOLLOWLOCATION一起使用的*/
+    curl_setopt($ci, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ci, CURLINFO_HEADER_OUT, true);
+    /*curl_setopt($ci, CURLOPT_COOKIE, $Cookiestr); * *COOKIE带过去** */
+    $response = curl_exec($ci);
+    $requestinfo = curl_getinfo($ci);
+    $http_code = curl_getinfo($ci, CURLINFO_HTTP_CODE);
+    if ($debug) {
+        echo "=====post data======\r\n";
+        var_dump($postfields);
+        echo "=====info===== \r\n";
+        print_r($requestinfo);
+        echo "=====response=====\r\n";
+        print_r($response);
+    }
+    curl_close($ci);
+    return $response;
+    //return array($http_code, $response,$requestinfo);
+}
---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,104 @@
+<?php
+namespace Home\Controller;
+
+use Think\Controller;
+
+class IndexController extends Controller
+{
+    /**
+     * 对比面板
+     */
+    public function index()
+    {
+        $this->display('sharesTemp');
+    }
+
+    /**
+     * 股票测试面板
+     */
+    public function sharesTemp()
+    {
+        $this->display();
+    }
+
+    /**
+     * 通联股票接口API
+     * 通联数据
+     */
+    public function wmCloudSharesApi()
+    {
+        $url = "https://api.wmcloud.com/data/v1/api/market/getTickRTSnapshot.json";
+        $params = array(
+            "securityID" => '000001.XSHE,000421.XSHE,000631.XSHE,600111.XSHG,600271.XSHG,600551.XSHG',
+            "assetClass" => "", //第几页(每页20条数据),默认第1页
+            "exchangeCD" => "",
+            "field" => "ticker,shortNM,changePct,lastPrice,change",
+        );
+        $paramstring = http_build_query($params);
+        $uurl = $url . "?" . $paramstring;
+        $method = "GET";
+        $headers = array("authorization: Bearer 23e9fe34863cc400508f02f1c628851ec92676433b694d736e38ffe4b527f59a", "cache-control: no-cache");
+        $ret = httpRequest($uurl, $method, $postfields = null, $headers, $debug = false);
+        $ret_arr = json_decode($ret,true);
+        $this->ajaxReturn($ret_arr);
+    }
+
+    /**
+     * 新浪股票数据API
+     * 新浪科技
+     */
+    public function sinaSharesApi()
+    {
+        $url = "https://hq.sinajs.cn/list=s_sz000001,s_sz000421,s_sz000631,s_sh600111,s_sh600271,s_sh600551";
+        $method = "GET";
+        $headers = array();
+        $ret = httpRequest($url, $method, $postfields = null, $headers, $debug = false);
+        $ret = iconv('GB2312', 'UTF-8', $ret);
+        $ret_arr1 = explode(';', $ret);
+        array_pop($ret_arr1);
+        $shares_arr = array();
+        foreach ($ret_arr1 as $k1 => $v1) {
+            $temp_v1 = trim(substr($v1, 4));
+            $tem_arr1 = explode('=', $temp_v1);
+            $tem_arr3 = explode(',', $tem_arr1[1]);
+            $shares_arr[] = array(
+                'shares_name' => substr($tem_arr3[0], 1),
+                'shares_code' => substr($tem_arr1[0], 11),
+                'shares_price' => $tem_arr3[1],
+                'shares_rate' => $tem_arr3[3],
+            );
+        }
+        $result = array(
+            'code' => 1,
+            'data' => $shares_arr,
+            'msg' => 'success',
+        );
+        $this->ajaxReturn($result);
+    }
+
+    /**
+     * 阿里云股票数据API
+     * 网极网络科技
+     */
+    public function aliYunSharesApi()
+    {
+        $host = "http://stock.api51.cn";
+        $path = "/sort";
+        $method = "GET";
+        $appcode = "13d26095b46e49a3a0b2865e0a8ccc86";
+        $headers = array();
+        array_push($headers, "Authorization:APPCODE " . $appcode);
+        $querys = "data_count=6&en_prod_code=000001.SZ,000421.SZ,000631.SZ,600111.SS,600271.SS,600551.SS&fields=prod_name,last_px,px_change&sort_field_name=px_change_rate&start_pos=start_pos";
+        //$querys = "data_count=10&en_hq_type_code=SS.ESA&fields=fields&sort_field_name=px_change_rate&sort_type=0&start_pos=start_pos";
+        $url = $host . $path . "?" . $querys;
+        $ret = httpRequest($url, $method, $postfields = null, $headers, $debug = false);
+        $ret_arr = json_decode($ret, true);
+        array_shift($ret_arr['data']['sort']);
+        $result = array(
+            'code' => 1,
+            'data' => $ret_arr['data']['sort'],
+            'msg' => 'success',
+        );
+        $this->ajaxReturn($result);
+    }
+}
